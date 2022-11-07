ece506a3609d3a3f465bf13bd338ca632d6cd2fb
Mohamed ELkilany <mohamedsayedelkilany@gmail.com>
2022-11-07 16:11:07 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Elkilany on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
