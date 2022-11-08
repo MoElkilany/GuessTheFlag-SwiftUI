f629ae51321a5d717e7460ac40abdfd23d9efa06
Mohamed ELkilany <mohamedsayedelkilany@gmail.com>
2022-11-08 13:26:02 +0200

    Gradient types

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,23 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            VStack(spacing: 0){
-                Color.red
-                Color.blue
-            }
-            
-            VStack{
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!").foregroundColor(.secondary)
-                    .background(.ultraThinMaterial)
-                
-              
-            }
-        }
-        .ignoresSafeArea()
+        
+        //        ZStack{
+        //            VStack(spacing: 0){
+        //                Color.red
+        //                Color.blue
+        //            }
+        //
+        //            VStack{
+        //                Image(systemName: "globe")
+        //                    .imageScale(.large)
+        //                    .foregroundColor(.accentColor)
+        //                Text("Hello, world!").foregroundColor(.secondary)
+        //                    .background(.ultraThinMaterial)
+        //
+        //
+        //            }
+        //        }
+        //        .ignoresSafeArea()
         
         //        VStack {
         //            Image(systemName: "globe")
@@ -34,6 +35,27 @@ struct ContentView: View {
         //            Text("Hello, world!")
         //        }
         //        .padding()
+        
+        VStack{
+            LinearGradient(gradient: Gradient(colors: [.blue,.red]), startPoint: .top, endPoint: .bottom)
+            
+            LinearGradient(gradient: Gradient(stops: [
+                .init(color: .blue, location: 0.20),
+                .init(color: .red, location: 0.60),
+            ]), startPoint: .top, endPoint: .bottom)
+            
+            RadialGradient(stops: [
+                .init(color: .red, location: 0.12),
+                .init(color: .blue, location: 0.20),
+                .init(color: .pink, location: 0.44),
+                .init(color: .orange, location: 0.55)
+            ], center: .center, startRadius:50, endRadius: 100)
+            
+            AngularGradient(gradient: Gradient(colors: [.purple,.orange ,.pink, .blue]), center: .center)
+        }
+        
+        
+            .ignoresSafeArea()
     }
 }
 
